bf10151edb770f6806f11de151b9c4be4213afa8
AiNga04 <aingaspkt@gmail.com>
2024-12-15 15:24:01 +0700

    add source build

---
+++ assets/js/add.js
@@ -0,0 +1,19 @@
+// Header
+fetch("../common/header.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("header").innerHTML = data;
+  })
+  .catch((error) => {
+    console.error("Error loading header:", error);
+  });
+
+// Footer
+fetch("../common/footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
+  })
+  .catch((error) => {
+    console.error("Error loading footer:", error);
+  });
